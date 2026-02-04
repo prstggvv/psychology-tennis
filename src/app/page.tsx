551068d3dae8a3d5a78d1a2fc5e9eb498fbc7d1a
Ivan <vany4golos@yandex.ru>
2026-02-04 19:52:58 +0300

    feat: aboutme section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import './styles/index.css';
 import { classNames } from "@/shared/lib/classNames/classNames";
 import { Header } from "@/components/Header";
 import { Hero } from '@/components/Hero';
+import { AboutMe } from '@/components/AboutMe';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <div className={classNames('wrapper', {}, [])}>
         <Header />
         <Hero />
+        <AboutMe />
       </div>
     </div>
   );
---
+++ src/components/AboutMe/ui/AboutMe.tsx
@@ -0,0 +1,56 @@
+import cls from './AboutMe.module.css';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import Image from 'next/image';
+
+import AboutMeImage from '@/shared/assets/images/hero/mainHero.jpg';
+import { aboutMeData } from '../model/aboutMeData';
+import Button from '@/shared/ui/Button/Button';
+import ArrowSvg from '@/shared/assets/images/icons/arrow.svg';
+
+interface IAboutMeProps {
+  className?: string;
+}
+
+export const AboutMe = ({ className }: IAboutMeProps) => {
+  return (
+    <section className={classNames(cls.section, {}, [className ?? ''])}>
+      <div className={classNames(cls.container, {}, [])}>
+        <div className={classNames(cls.imageWrapper, {}, [])}>
+          <Image
+            src={AboutMeImage}
+            alt='Картинка'
+            className={classNames(cls.image, {}, [])}
+          />
+        </div>
+        <h2 className={classNames(cls.headingMobile, {}, [])}>
+          Обо мне
+        </h2>
+        <div className={classNames(cls.mainInfo, {}, [])}>
+          <h2 className={classNames(cls.heading, {}, [])}>
+            Обо мне
+          </h2>
+          <div className={classNames(cls.main, {}, [])}>
+            <p className={classNames(cls.text, {}, [])}>
+              Меня зовут Еркеева (Чернышева) Александра и я являюсь не
+              только спортивным тренером с опытом в работы в 10 лет,
+              но и спортивным психологом
+            </p>
+          </div>
+          <Button
+            className={classNames(cls.button, {}, [])}
+            type='button'
+          >
+            <Image
+              className={classNames(cls.svg, {}, [])}
+              src={ArrowSvg}
+              alt='стрелка'
+            />
+            <p className={classNames(cls.textButton, {}, [])}>
+              Записаться
+            </p>
+          </Button>
+        </div>
+      </div>
+    </section>
+  )
+}
